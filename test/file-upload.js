1c4ef01fbacab239307fad68a9db4e5f389027ee
Meena.Hoda <meena.hoda@wmfs.net>
2018-08-01 15:01:33 +0100

    test: more coverage

--- test/file-upload.js
+++ test/file-upload.js
@@ -8,8 +8,8 @@ const fs = require('fs')
 const expect = require('chai').expect
 
 const UPLOAD_FILE_STATE_MACHINE = 'tymly_uploadFile_1_0'
-const FILENAME = 'fixtures/test_file.txt'
-const FILEPATH = path.resolve(__dirname, FILENAME)
+const fileName = 'fixtures/test_file.txt'
+const FILEPATH = path.resolve(__dirname, fileName)
 let fileModel, fileId, statebox, tymlyService, base64
 
 describe('file upload tests', function () {
@@ -31,14 +31,14 @@ describe('file upload tests', function () {
       }
     )
   })
-  
+
   it('read the file in so we can pass it on', async () => {
     base64 = fs.readFileSync(FILEPATH).toString('base64')
   })
 
   it('should start the state machine to get file Upload data', async () => {
     const execDesc = await statebox.startExecution(
-      {base64, fileName: 'fixtures/test_file.txt'},
+      {base64, fileName},
       UPLOAD_FILE_STATE_MACHINE,
       {sendResponse: 'COMPLETE', userId: 'test-user'}
     )
@@ -47,7 +47,7 @@ describe('file upload tests', function () {
     expect(execDesc.currentResource).to.eql('module:uploadFile')
     expect(execDesc.stateMachineName).to.eql(UPLOAD_FILE_STATE_MACHINE)
     expect(execDesc.status).to.eql('SUCCEEDED')
-    expect(execDesc.ctx.fileName).to.eql(FILENAME)
+    expect(execDesc.ctx.fileName).to.eql(fileName)
 
     fileId = execDesc.ctx.fileId
   })
@@ -55,12 +55,12 @@ describe('file upload tests', function () {
   it('should check that the file has been uploaded', async () => {
     const doc = await fileModel.findById(fileId)
 
-    expect(doc.fileName).to.eql(FILENAME)
+    expect(doc.fileName).to.eql(fileName)
     expect(doc.id).to.eql(fileId)
     expect(doc.createdBy).to.eql('test-user')
   })
 
-  it('should start the state machine without any input/file', async () => {
+  it('should start the state machine without any file name', async () => {
     const execDesc = await statebox.startExecution(
       {},
       UPLOAD_FILE_STATE_MACHINE,
@@ -69,6 +69,17 @@ describe('file upload tests', function () {
 
     expect(execDesc.status).to.eql('FAILED')
     expect(execDesc.errorCode).to.eql('No fileName on input')
+  })
+
+  it('should start the state machine without any base64', async () => {
+    const execDesc = await statebox.startExecution(
+      {fileName},
+      UPLOAD_FILE_STATE_MACHINE,
+      {sendResponse: 'COMPLETE', userId: 'test-user'}
+    )
+
+    expect(execDesc.status).to.eql('FAILED')
+    expect(execDesc.errorCode).to.eql('No base64 on input')
   })
 
   it('should shut down Tymly nicely', async () => {
